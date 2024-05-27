8a44e5268e9a11362ade6c072969fc1dc645a376
prs2210 <testuser22seller@gmail.com>
2024-05-27 19:07:31 +0530

    Add project

---
+++ cypress.config.js
@@ -0,0 +1,37 @@
+const { defineConfig } = require("cypress");
+
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+
+async function setupNodeEvents(on, config) {
+  //require('cypress-mochawesome-reporter/plugin')(on);
+
+  await addCucumberPreprocessorPlugin(on, config);
+  on(
+    "file:preprocessor",
+    createBundler({
+      plugins: [createEsbuildPlugin.default(config)],
+    })
+  );
+
+  require('cypress-mochawesome-reporter/plugin')(on);
+ 
+  return config;
+}
+
+module.exports = defineConfig({
+  defaultCommandTimeout: 8000,
+  reporter: 'cypress-mochawesome-reporter',
+  video: true,
+  screenshotsFolder: "images",
+  env: {
+    url: "https://rahulshettyacademy.com/"
+  },
+  e2e: {
+    setupNodeEvents,
+    specPattern: ['cypress/e2e/**/*.feature', 'cypress/e2e/**/*.js']
+  },
+});
